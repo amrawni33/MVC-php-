7dd532c8183f3730bdc058c3ec671afc905f0ca8
Amr Awni AbdelTawab Youssef <83365437+amrawni33@users.noreply.github.com>
2022-11-16 01:35:48 +0200

    first commit

---
+++ src/App.php
@@ -0,0 +1,9 @@
+/**FRONT END CONTROLLER */
+
+<?php
+ 
+    class App{
+        protected $controller = "homeController";
+        protected $action = "index";
+        protected $params = [];
+    }
